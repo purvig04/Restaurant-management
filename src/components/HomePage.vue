f999ccffdd178af4cd58e87853d813652d31942f
purvi <purvig@id.argusoft.com>
2025-09-10 12:14:07 +0530

    add homePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Home Component</h1>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  mounted(){
+        let user=localStorage.getItem('user-info')
+        if(!user){
+            this.$router.push({name:'SignUp'})
+        }
+        
+    }
+};
+</script>
