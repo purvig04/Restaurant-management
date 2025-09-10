51b2eeac5180df472b9ffac5b3de8ef1036d2e8d
purvi <purvig@id.argusoft.com>
2025-09-10 10:21:05 +0530

    add signup component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,33 @@
+<template>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>SignUp</h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter name"/>
+        <input type="text" v-model="email" placeholder="Enter Email"/>
+        <input type="password" v-model="password" placeholder="Enter Password"/>
+        <button type="submit" v-on:click="signUp">Submit</button>
+    </div> 
+   
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: 'SignUp',
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        signUp(){
+            console.log(this.name,this.email,this.password);
+            
+        }
+    }
+  
+  }
+  </script>
