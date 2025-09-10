ca0ca2f25a780664e05e96afe1867b2bbe53a61c
purvi <purvig@id.argusoft.com>
2025-09-10 12:29:13 +0530

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Restaurant</a>
+        <a href="#">Update Restaurant</a>
+        <a href="#">Logout</a>
+    </div>
+</template>
+<script>
+export default{
+    name:'HeaderComp'
+}</script>
+
+<style>
+.nav{
+    background-color: #333;
+    overflow: hidden;
+}
+
+.nav a{
+    float: left;
+    color: #f2f2f2;
+    padding: 16px 14px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    margin-right: 4px;
+}
+
+.nav a:hover{
+    background-color: #ddd;
+    color: #222;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,16 @@
 <template>
+  <HeaderComp/>
   <h1>Home Component</h1>
+  
 </template>
 
 <script>
+import HeaderComp from "./Header.vue";
 export default {
   name: 'HomePage',
+  components:{
+    HeaderComp
+  },
   mounted(){
         let user=localStorage.getItem('user-info')
         if(!user){
