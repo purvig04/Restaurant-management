bcb26e7505843456265c1de6e51d40ed4a6e7512
purvi <purvig@id.argusoft.com>
2025-09-10 16:39:59 +0530

    adding new restaurant added

---
+++ src/components/AddRestro.vue
@@ -0,0 +1,54 @@
+<template>
+    <HeaderComp/>
+    <h1>Add Restro Page</h1>
+    <form class="add">
+      <input type="text" name="name" v-model="restaurant.name" placeholder="Enter New Restaurant"/>
+      <input type="text" name="addres" v-model="restaurant.address" placeholder="Enter Address"/>
+      <input type="text" name="contact" v-model="restaurant.contact"  placeholder="Enter Contact no"/>
+      <button type="button" v-on:click="addRestro">Add new Restaurant</button>
+
+    </form>
+    
+  </template>
+  
+  <script>
+
+import HeaderComp from "./Header.vue";
+import axios from "axios";
+  export default {
+    name: ' AddRestro',
+    components:{
+      HeaderComp
+    },
+    data(){
+      return{
+        restaurant:{
+          name:'',
+          address:'',
+          contact:''
+        }
+      }
+    },
+    methods:{
+      async addRestro(){
+        const result =await axios.post("http://localhost:3000/restaurants" , {
+          name:this.restaurant.name,
+          address:this.restaurant.address,
+          contact:this.restaurant.contact
+        })
+         if(result.status==201){
+          this.$router.push({name:'Home'})
+         }
+        console.warn(this.restaurant)
+      }
+    },
+    mounted(){
+          let user=localStorage.getItem('user-info')
+          if(!user){
+              this.$router.push({name:'SignUp'})
+          }
+          
+      }
+  };
+  </script>
+  
